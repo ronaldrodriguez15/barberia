1f26786f10eb01ada19b1f9c33754a3676285502
ronaldrodriguez15 <80416054+ronaldrodriguez15@users.noreply.github.com>
2022-12-06 19:42:22 -0500

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="back-dark" style="width:100% !important; height:100% !important"></div>
 <div class="container">
-    <div class="row justify-content-center mt-5">
+    <div class="row justify-content-center mt-5 mb-5" style="margin-top: 100px !important">
         <div class="col-md-8"> 
             <div class="row justify-content-center">
                 <div class="col-md-10">
-                    <div class="card">
-                        <div class="card-header">
-                            {{ __('Login') }}
-                        </div>
+                    <div class="text-center mb-5">
+                        <img src="{{ asset('img/Logo.png') }}" alt="..." width="150" class="rounded-circle">
+                    </div>
+                    <div class="card rounded-3 shadow-lg">
                         <div class="card-body">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
@@ -18,27 +19,33 @@
                                     <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo electronico') }}</label>
         
                                     <div class="col-md-6">
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-envelope"></i></span>
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="alguien@example.com" autofocus>
+            
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
                                     </div>
                                 </div>
         
                                 <div class="row mb-3 mt-5">
                                     <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
         
-                                    <div class="col-md-6">
-                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-        
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                    <div class="col-md-6"> 
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-lock"></i></span>
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="password...">
+            
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
                                     </div>
                                 </div>
         
@@ -54,14 +61,16 @@
         
                                 <div class="row justify-content-center mb-0 mt-5">
                                     <div class="col-md-8 text-end">
-                                        <button type="submit" class="btn btn-success">
-                                            {{ __('Ingresar') }}
+
+                                        <a class="btn btn-dark" href="{{ route('welcome') }}">{{ __('Atrás') }}</i></a>
+
+                                        <button type="submit" class="btn btn-success color-secondary">
+                                            {{ __('Ingresar') }} <i class="fa-solid fa-right-to-bracket ms-2"></i>
                                         </button>
         
                                         {{-- @if (Route::has('register'))
                                             <a class="btn btn-primary" href="{{ route('register') }}">{{ __('Registrate') }}</a>
                                         @endif --}}
-                                        <a class="btn btn-primary" href="{{ route('welcome') }}">{{ __('Atrás') }}</a>
                                     </div>
                                 </div>
                             </form>
